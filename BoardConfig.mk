7746c312de6e33f8251285b95c96145cccd25ff9
wimpknocker <wimpknocker@hotmail.com>
2014-11-21 19:40:46 +0200

    TWRP Recovery Build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,10 +119,22 @@ BOARD_LPM_BOOT_ARGUMENT_VALUE := 1
 # Recovery
 BOARD_UMS_LUNFILE := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun0/file"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun%d/file"
+
+
+# TWRP flags
+DEVICE_RESOLUTION := 480x800
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_FSTAB := device/samsung/skomer/rootdir/fstab.samsungskomer
-RECOVERY_FSTAB_VERSION := 2
-BOARD_RECOVERY_SWIPE := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/skomer/recovery/graphics.c
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_SUPPRESS_EMMC_WIPE := true
+TW_HAS_NO_RECOVERY_PARTITION := true
+TW_FLASH_FROM_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/storage/sdcard1"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "/storage/sdcard1"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_INCLUDE_FUSE_EXFAT := false
+TW_BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/skomer/recovery/twrp-graphics.c
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_NO_USB_STORAGE := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
