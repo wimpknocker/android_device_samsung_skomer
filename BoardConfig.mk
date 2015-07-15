771d82f073e47faec9fe74394050c152d30be0cc
wimpknocker@wimpnether <wimpknocker@wimpnether.net>
2015-07-15 18:34:09 +0300

    skomer: new cm-12.1

---
+++ BoardConfig.mk
@@ -0,0 +1,97 @@
+#
+# Copyright (C) 2015 Wimpknocker
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a9
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5064622080
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Board
+TARGET_BOOTLOADER_BOARD_NAME := montblanc
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform 
+TARGET_BOARD_PLATFORM := montblanc
+BOARD_USES_STE_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DSTE_HARDWARE -DSTE_SAMSUNG_HARDWARE 
+BOARD_LPM_BOOT_ARGUMENT_NAME := lpm_boot
+BOARD_LPM_BOOT_ARGUMENT_VALUE := 1
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+TARGET_KERNEL_SOURCE := kernel/samsung/skomer
+TARGET_KERNEL_CONFIG := cyanogenmod_skomer_defconfig
+TARGET_NO_INITLOGO := true
+
+# Graphics
+USE_OPENGL_RENDERER := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+BOARD_EGL_CFG := device/samsung/skomer/prebuilt/lib/egl/egl.cfg
+COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH -DWORKAROUND_BUG_10194508
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+BOARD_HAVE_PRE_KITKAT_AUDIO_BLOB := true
+BOARD_HAVE_PRE_KITKAT_AUDIO_POLICY_BLOB := true
+COMMON_GLOBAL_CFLAGS += -DMR0_AUDIO_BLOB -DMR1_AUDIO_BLOB
+USE_LEGACY_AUDIO_POLICY := 1
+BOARD_USES_LEGACY_MMAP := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/skomer/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/skomer/bluetooth/btvendor_skomer.txt
+
+# Wi-Fi related defines
+BOARD_WLAN_DEVICE := bcmdhd
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/system/etc/wifi/bcmdhd_p2p.bin"
+WIFI_DRIVER_MODULE_NAME := "dhd"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+BOARD_NO_APSME_ATTR := true
+
+# Misc
+BOARD_RIL_CLASS := ../../../device/samsung/skomer/ril/
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/skomer/include
+TARGET_RECOVERY_FSTAB := device/samsung/skomer/ramdisk/fstab.samsungskomer
+
+# GPS
+TARGET_ENABLE_NON_PIE_SUPPORT := true
---
+++ device.mk
@@ -0,0 +1,105 @@
+#
+# Copyright (C) 2015 Wimpknocker
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Add overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/skomer/overlay
+
+# Define kind of DPI
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+# Hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Wi-Fi packages
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    dhcpcd.conf \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# Open-source HALs
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.r_submix.default \
+    lights.montblanc
+
+# Misc packages
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs \
+    Stk
+
+#F2FS
+PRODUCT_PACKAGES += \
+    genfstab
+
+# Prebuilt kernel
+PRODUCT_COPY_FILES += \
+    device/samsung/skomer/ramdisk/zImage:kernel
+
+# Initial ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/skomer/ramdisk/init.samsungskomer.rc:root/init.samsungskomer.rc \
+    device/samsung/skomer/ramdisk/init.samsungskomer.usb.rc:root/init.samsungskomer.usb.rc \
+    device/samsung/skomer/ramdisk/ueventd.samsungskomer.rc:root/ueventd.samsungskomer.rc \
+    device/samsung/skomer/ramdisk/lpm.rc:root/lpm.rc \
+    device/samsung/skomer/ramdisk/sbin/charger:root/sbin/charger \
+    device/samsung/skomer/ramdisk/res/images/charger/battery_0.png:root/res/images/charger/battery_0.png \
+    device/samsung/skomer/ramdisk/res/images/charger/battery_1.png:root/res/images/charger/battery_1.png \
+    device/samsung/skomer/ramdisk/res/images/charger/battery_2.png:root/res/images/charger/battery_2.png \
+    device/samsung/skomer/ramdisk/res/images/charger/battery_3.png:root/res/images/charger/battery_3.png \
+    device/samsung/skomer/ramdisk/res/images/charger/battery_4.png:root/res/images/charger/battery_4.png \
+    device/samsung/skomer/ramdisk/res/images/charger/battery_5.png:root/res/images/charger/battery_5.png \
+    device/samsung/skomer/ramdisk/res/images/charger/battery_charge.png:root/res/images/charger/battery_charge.png \
+    device/samsung/skomer/ramdisk/res/images/charger/battery_fail.png:root/res/images/charger/battery_fail.png
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+# Codec config files
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml
+
+# Every single binary blob
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/samsung/skomer/prebuilt,system)
+
+# Wi-Fi config
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
+
+# Dalvik config
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
